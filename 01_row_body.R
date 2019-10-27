42aa95e72a19d17c069e7b69ff6b1b79e5b3a3b6
takewiki <hulilei@takewiki.com.cn>
2019-10-27 16:41:27 +0800

    here to go! come on!

--- 01_row_body.R
+++ 01_row_body.R
@@ -1,6 +1,6 @@
 menu_row <- tabItem(tabName = "row",
                     fluidRow(
-                      column(width = 4,
+                      column(width = 3,
                              box(
                                title = "步骤0：品牌选择", width = NULL, solidHeader = TRUE, status = "primary",
                                
@@ -12,33 +12,44 @@ menu_row <- tabItem(tabName = "row",
                              )
                       ),
                       
-                      column(width = 4,
+                      column(width = 5,
                              box(
-                               title = "步骤1:问答对全量ID化", width = NULL, solidHeader = TRUE, status = "primary",
-                               actionButton('nsup_qalist_idize','问答对全量ID化')
+                               title = "步骤1:问答对全量ID化", width = NULL, solidHeader = TRUE, status = "primary"
+                               ,div("提示:目前采用全量更新的方式对性能还是有些影响,由于不涉及版本化，可能考虑使用增量更新")
+                               #,div("")
+                               ,br()
+                               ,actionButton('nsup_qalist_idize','QA对ID化')
                              ),
                              box(
-                               title = "步骤2:NSCL全量更新", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
+                               title = "步骤2:NSCL全量更新", width = NULL, solidHeader = TRUE, status = "primary"
+                               ,div("提示:此步骤主要应用关键词删除词库对ID化后的语料进行处理")
+                               ,br()
+                               ,actionButton('nsup_nscl_update','NSCL全量更新')
                              ),
                              box(
-                               title = "步骤3:NSBL全量更新", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
+                               title = "步骤3:NSBL全量更新", width = NULL, solidHeader = TRUE, status = "primary"
+                               ,div("提示:此步骤主要应用文件级黑名单,问题级黑名单,答案级黑名单针对语料进行处理")
+                               ,br()
+                               ,actionButton('nsup_nsbl_update','NSBL全量更新 ')
                              )
                       ),
                       
                       column(width=4,
                              box(
-                               title = "步骤4:NSDICT全量更新", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
+                               title = "步骤4:NSDICT全量更新", width = NULL, solidHeader = TRUE, status = "primary"
+                               ,div("提示:更新分词")
+                               ,actionButton('nsup_nsdict_update','NSDICT全量更新')
+                               
                              ),
                              box(
-                               title = "步骤5:NSCS全量更新", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
+                               title = "步骤5:NSCS全量更新", width = NULL, solidHeader = TRUE, status = "primary"
+                               ,div('提示：更新聚类')
+                               ,actionButton('nsup_nscs_update','NSCS全量更新')
                              ),
                              box(
-                               title = "步骤6:处理结果下载", width = NULL, solidHeader = TRUE, status = "primary",
-                               "Box content"
+                               title = "步骤6:处理结果下载", width = NULL, solidHeader = TRUE, status = "primary"
+                               ,div('提示:下载数据适配来也')
+                               ,mdl_download_button('nsup_output_download','下载文件,更新知识库')
                              )
                       )
                     )
--- server.R
+++ server.R
@@ -3,9 +3,12 @@
 #shinyserver start point----
  shinyServer(function(input, output,session) {
    
+   #0 共享品牌选择------
+   
+   brand <- var_ListChoose1('nsup_brand_sel');
    
    #1.问答全量ID    ------
-   brand <- var_ListChoose1('nsup_brand_sel');
+
    
    observeEvent(input$nsup_qalist_idize,{
 
@@ -18,10 +21,44 @@
         msg <-nsclpkg::qalist_idize(brand)
         pop_notice(msg)
 
-
-
-
      })
+   
+   #2 NSCL全量更新--------
+   observeEvent(input$nsup_nscl_update,{
+     #code here
+      brand <- brand();
+      nsblpkg::nsim_nscl_version(brand); 
+      pop_notice('NSCL更新完成')
+     
+     #code end
+   })
+   
+   #3 NSBL全量更新--------
+   observeEvent(input$nsup_nsbl_update,{
+     #code here
+       brand <- brand();
+      nsblpkg::nsim_nsbl_version(brand)
+       pop_notice('NSBL更新完成')
+     #code end
+   })
 
-  
+  #4 更新NSDICT------
+   observeEvent(input$nsup_nsdict_update,{
+      #code here
+      
+      
+      
+      #code end
+      
+   })
+   
+ #5.更新NSCS-------
+   observeEvent(input$nsup_nscs_update,{
+      #code here
+      
+      #code end
+   })
+#6下载文件----
+   
+   run_download_button(proc_download_button,id = 'nsup_output_download',data = iris)
 })
